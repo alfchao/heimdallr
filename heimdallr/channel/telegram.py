0f29133be27fb17f6499eedc51cbc9521c9fa72c
alfred <1435414174@qq.com>
2026-09-04 08:20:23 +0800

    feat: 支持Telegram API BaseURL配置

--- heimdallr/channel/telegram.py
+++ heimdallr/channel/telegram.py
@@ -5,7 +5,11 @@ import requests
 
 from heimdallr.channel.base import Channel, Message
 from heimdallr.config.config import get_config_str
-from heimdallr.config.definition import SUFFIX_TELEGRAM_CHAT_ID, SUFFIX_TELEGRAM_TOKEN
+from heimdallr.config.definition import (
+    SUFFIX_TELEGRAM_BASE_URL,
+    SUFFIX_TELEGRAM_CHAT_ID,
+    SUFFIX_TELEGRAM_TOKEN,
+)
 from heimdallr.exception import ParamException
 
 logger = logging.getLogger(__name__)
@@ -25,7 +29,7 @@ class TelegramMessage(Message):
 class Telegram(Channel):
     def __init__(self, name: str, type: str):
         super().__init__(name, type)
-        self.base_url = "https://api.telegram.org/bot"
+        self.base_url: str
         self.token: str
         self.chat_id: str
         self._build_channel()
@@ -33,6 +37,11 @@ class Telegram(Channel):
     def _build_channel(self) -> None:
         self.token = get_config_str(self.get_name(), SUFFIX_TELEGRAM_TOKEN, "")
         self.chat_id = get_config_str(self.get_name(), SUFFIX_TELEGRAM_CHAT_ID, "")
+        self.base_url = get_config_str(
+            self.get_name(),
+            SUFFIX_TELEGRAM_BASE_URL,
+            "https://api.telegram.org/bot",
+        )
         if not self.token or not self.chat_id:
             raise ParamException("Telegram token or chat id is empty.")
 
